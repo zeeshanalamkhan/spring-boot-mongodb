af3c16639c5ea14a2343658c05e6b1cf146e6f26
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-10-20 19:19:50 +0530

    BookNotFoundException  class with logging

--- spring-boot-mongodb/src/main/java/com/zeeshan/exception/BookNotFoundException.java
+++ spring-boot-mongodb/src/main/java/com/zeeshan/exception/BookNotFoundException.java
@@ -1,8 +1,13 @@
 package com.zeeshan.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class BookNotFoundException extends Exception {
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = LogManager.getLogger(BookNotFoundException.class);
 
 	public BookNotFoundException() {
 		super();
