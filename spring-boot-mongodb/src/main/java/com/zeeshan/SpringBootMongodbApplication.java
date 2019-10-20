b13643b2466e8aca609261fc3f8dfcba00f0b35a
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-10-20 19:14:40 +0530

    main method class with logging

--- spring-boot-mongodb/src/main/java/com/zeeshan/SpringBootMongodbApplication.java
+++ spring-boot-mongodb/src/main/java/com/zeeshan/SpringBootMongodbApplication.java
@@ -1,13 +1,23 @@
 package com.zeeshan;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootMongodbApplication {
 
+	private static final Logger logger = LogManager.getLogger(SpringBootMongodbApplication.class);
+
 	public static void main(String[] args) {
+
+		logger.info("SpringBootMongodbApplication class execution started");
+
 		SpringApplication.run(SpringBootMongodbApplication.class, args);
+
+		logger.info("SpringBootMongodbApplication execution finished");
+		
 	}
 
 }
