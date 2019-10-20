4969b9dfc66470bf35c2699dffd868bab92b25fa
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-10-20 19:18:54 +0530

    BookExceptionHandler class with logging

--- spring-boot-mongodb/src/main/java/com/zeeshan/exception/BookExceptionHandler.java
+++ spring-boot-mongodb/src/main/java/com/zeeshan/exception/BookExceptionHandler.java
@@ -2,6 +2,8 @@ package com.zeeshan.exception;
 
 import java.util.Date;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,13 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @ControllerAdvice
 public class BookExceptionHandler {
 
+	private static final Logger logger = LogManager.getLogger(BookExceptionHandler.class);
+	
 	@ExceptionHandler(value = BookNotFoundException.class)
 	public ResponseEntity<BookError> noBookHandler(BookNotFoundException ex) {
 
+		logger.info("noBookHandler() execution started");
 		BookError error = new BookError();
 		error.setErrCode(404);
 		error.setDesc(ex.getMessage());
 		error.setDate(new Date());
+		logger.info("noBookHandler() execution finished");
 		return new ResponseEntity<BookError>(error, HttpStatus.NOT_FOUND);
 
 	}
@@ -26,7 +32,9 @@ public class BookExceptionHandler {
 	@ExceptionHandler(BookSaveException.class)
 	public ResponseEntity<BookError> bookNotSaved(NullPointerException ex) {
 
+		logger.info("bookNotSaved() execution started");
 		BookError error = new BookError(400, ex.getMessage(), new Date());
+		logger.info("bookNotSaved() execution finished");
 		return new ResponseEntity<BookError>(error, HttpStatus.BAD_REQUEST);
 
 	}
